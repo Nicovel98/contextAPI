1e3cceb86d3217132df7ade823217164e5dcef51
Nicovel <nicovel98@gmail.com>
2024-07-24 07:37:53 -0500

    Validaciones en el login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,36 +1,62 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { Context } from "../context/Context";
 
 export const Login = () => {
 
   // Extraemos user y setUser del contexto usando useContext
   const { user, setUser } = useContext(Context);
+  const [username, setUsername] = useState('');
+  const [name, setName] = useState('');
+  const [phone, setPhone] = useState('');
 
+  // Método para validar el campo username: no admite espacios en blanco ni mayúsculas
+  const handleUsernameV = e => {
+    const value = e.target.value.replace(/\s+/g, '').toLowerCase();
+    setUsername(value);
+  };
+
+  // Método para validar el nombre y cambiar la primera a mayúsculas
+  const handleNameV = e => {
+    const value = e.target.value
+      .split(' ')
+      .map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
+      .join(' ');
+    setName(value);
+  };
+
+  // Método para validar que se escriban solo números en el campo teléfono
+  const handlePhoneV = e => {
+    const value = e.target.value.replace(/[^0-9]/g, '');
+    setPhone(value);
+  }
+
+  // Método que se ejecuta al hacer clic en enviar en el formulario
   const saveData = e => {
-    // Evitar que el formulario se envíe por defecto
     e.preventDefault();
 
     // Crear el objeto del usuario a partir de los datos del formulario
     let identified_user = {
       username: e.target.username.value,
       name: e.target.name.value,
-      phone: e.target.phone.value,
+      phone: e.target.phone.value
     }
+
     console.log(identified_user);
-    // Actualizamos el estado de user con los datos del nuevo usuario
+
+    // Actualizamos el estado del usuario
     setUser(identified_user);
   };
+
 
   return (
     <div>
       <h1 className="text-center">Login</h1>
       <form className="login" onSubmit={saveData}>
-        <input type="text" name="username" placeholder="username" />
-        <input type="text" name="name" placeholder="name" />
-        <input type="text" name="phone" placeholder="phone" />
+        <input type="text" name="username" placeholder="username" value={username} onChange={handleUsernameV} required />
+        <input type="text" name="name" placeholder="name" value={name} onChange={handleNameV} required />
+        <input type="text" name="phone" placeholder="phone" value={phone} onChange={handlePhoneV} required />
         <input type="submit" value="Enviar" />
       </form>
-      <p>Esta es la página de login</p>
     </div>
   )
 }
